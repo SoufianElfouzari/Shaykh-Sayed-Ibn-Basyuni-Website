79e5c3afd17c5f415b95ff73ce27969bdba90266
Wierss Programm <elfouzari.soufian@gmail.com>
2026-07-22 00:59:13 +0200

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
