4b9c05fe59a498fee354d4005ba8364ef1d1a1ef
Wierss Programm <elfouzari.soufian@gmail.com>
2026-07-22 01:51:04 +0200

    feat: add hero section to home structure

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,14 @@
 import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
 
 function Home() {
   return (
     <div className="home">
         <Header />
+
+        <main>
+            <Hero />
+        </main>
     </div>
   );
 }
