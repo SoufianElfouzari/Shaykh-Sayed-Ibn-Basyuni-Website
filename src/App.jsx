07469461c67ec91d55e9c1f969f41d160bcbd2b0
Wierss Programm <elfouzari.soufian@gmail.com>
2026-07-22 01:03:23 +0200

    feat: add Home Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app">
         <Routes>
+          <Route path="/" element={<Home />} />
         </Routes>
       </div>
     </BrowserRouter>
