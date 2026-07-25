bad599655459536c1232a5d234ebf005f6ab70c8
Wierss Programm <elfouzari.soufian@gmail.com>
2026-07-25 20:11:09 +0200

    feat: add articles route to site

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Shaykh from "./pages/Shaykh/Shaykh";
 import Duruus from "./pages/Duruus/Duruus";
+import Articles from "./pages/Articles/Articles";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/shaykh" element={<Shaykh />} />
           <Route path="/duruus" element={<Duruus />} />
+          <Route path="/articles" element={<Articles />} />
         </Routes>
       </div>
     </BrowserRouter>
