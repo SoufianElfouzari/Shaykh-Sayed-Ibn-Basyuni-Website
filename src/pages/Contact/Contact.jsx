13839b5547d2ab914def3da43d2422ac4eaa5fa0
Wierss Programm <elfouzari.soufian@gmail.com>
2026-07-25 20:57:19 +0200

    feat: add inital contact page structure

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,17 @@
+import Footer from "../common/Footer/Footer";
+import Header from "../common/Header/Header";
+
+function Contact() {
+  return (
+    <div className="contact">
+        <Header />
+
+        <main>
+        </main>
+
+        <Footer />
+    </div>
+  );
+}
+
+export default Contact;
