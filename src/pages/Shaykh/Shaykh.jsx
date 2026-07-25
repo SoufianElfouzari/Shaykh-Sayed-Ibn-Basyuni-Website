737bc609501ed924f0fb3b28af212e5642b2ebb0
Wierss Programm <elfouzari.soufian@gmail.com>
2026-07-25 18:06:28 +0200

    feat: add inital Shaykh Page

---
+++ src/pages/Shaykh/Shaykh.jsx
@@ -0,0 +1,17 @@
+import Footer from "../Home/components/Footer/Footer";
+import Header from "../Home/components/Header/Header";
+
+function Shaykh() {
+  return (
+    <div className="shaykh">
+        <Header />
+
+        <main>
+        </main>
+
+        <Footer />
+    </div>
+  );
+}
+
+export default Shaykh;
