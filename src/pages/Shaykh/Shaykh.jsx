7620b4e7e102069293e17179513c10d270da91b3
Wierss Programm <elfouzari.soufian@gmail.com>
2026-07-25 18:36:05 +0200

    feat: add hero to home structure

--- src/pages/Shaykh/Shaykh.jsx
+++ src/pages/Shaykh/Shaykh.jsx
@@ -1,5 +1,6 @@
 import Footer from "../Home/components/Footer/Footer";
 import Header from "../Home/components/Header/Header";
+import ShaykhHero from "./components/ShaykhHero/ShaykhHero";
 
 function Shaykh() {
   return (
@@ -7,6 +8,7 @@ function Shaykh() {
         <Header />
 
         <main>
+            <ShaykhHero />
         </main>
 
         <Footer />
