f172c7453e31759236ebadbc88c09d09c4bc3f5d
Wierss Programm <elfouzari.soufian@gmail.com>
2026-07-25 19:27:53 +0200

    feat: add lessen schedule to duruus structure

--- src/pages/Duruus/Duruus.jsx
+++ src/pages/Duruus/Duruus.jsx
@@ -1,5 +1,7 @@
 import Footer from "../Home/common/Footer/Footer";
 import Header from "../Home/common/Header/Header";
+import LessonSchedule from "../Home/common/LessonSchedule/LessonSchedule";
+import DuruusHero from "./DuruusHero/DuruusHero";
 
 function Duruus() {
   return (
@@ -7,7 +9,8 @@ function Duruus() {
         <Header />
 
         <main>
-
+            <DuruusHero />
+            <LessonSchedule />
         </main>
 
         <Footer />
