d1521ae29a63c19f212c552e2631009ab6f73bd9
Wierss Programm <elfouzari.soufian@gmail.com>
2026-07-25 20:10:39 +0200

    feat: add articles inital commit

---
+++ src/pages/Articles/Articles.jsx
@@ -0,0 +1,17 @@
+import Footer from "../Home/common/Footer/Footer";
+import Header from "../Home/common/Header/Header";
+
+function Articles() {
+  return (
+    <div className="articles">
+        <Header />
+
+        <main>
+        </main>
+
+        <Footer />
+    </div>
+  );
+}
+
+export default Articles;
