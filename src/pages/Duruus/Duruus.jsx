94938f10f150baa63fd31bc1a58d190a34767c41
Wierss Programm <elfouzari.soufian@gmail.com>
2026-07-25 19:44:41 +0200

    feat: add notices banner to duruus page structure

--- src/pages/Duruus/Duruus.jsx
+++ src/pages/Duruus/Duruus.jsx
@@ -2,6 +2,7 @@ import Footer from "../Home/common/Footer/Footer";
 import Header from "../Home/common/Header/Header";
 import LessonSchedule from "../Home/common/LessonSchedule/LessonSchedule";
 import DuruusHero from "./DuruusHero/DuruusHero";
+import NoticesBanner from "./NoticesBanner/NoticesBanner";
 
 function Duruus() {
   return (
@@ -10,6 +11,7 @@ function Duruus() {
 
         <main>
             <DuruusHero />
+            <NoticesBanner />
             <LessonSchedule />
         </main>
 
