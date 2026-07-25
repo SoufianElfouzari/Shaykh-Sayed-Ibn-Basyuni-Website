7829faa1bd76884cd4585651082019e913b665d0
Wierss Programm <elfouzari.soufian@gmail.com>
2026-07-25 19:27:22 +0200

    feat: initilize and add duruus route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Shaykh from "./pages/Shaykh/Shaykh";
+import Duruus from "./pages/Duruus/Duruus";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shaykh" element={<Shaykh />} />
+          <Route path="/duruus" element={<Duruus />} />
         </Routes>
       </div>
     </BrowserRouter>
