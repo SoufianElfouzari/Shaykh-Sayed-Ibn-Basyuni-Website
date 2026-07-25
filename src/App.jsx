69a5aeae29b8af9d448baca35e27c8bfd0e95c39
Wierss Programm <elfouzari.soufian@gmail.com>
2026-07-25 20:50:28 +0200

    feat: add article detail slug route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Shaykh from "./pages/Shaykh/Shaykh";
 import Duruus from "./pages/Duruus/Duruus";
 import Articles from "./pages/Articles/Articles";
+import ArticleDetails from "./pages/Articles/components/ArticleDetails/ArticleDetails";
+
 
 function App() {
   return (
@@ -11,9 +17,26 @@ function App() {
       <div className="app">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/shaykh" element={<Shaykh />} />
-          <Route path="/duruus" element={<Duruus />} />
-          <Route path="/artikel" element={<Articles />} />
+
+          <Route
+            path="/shaykh"
+            element={<Shaykh />}
+          />
+
+          <Route
+            path="/duruus"
+            element={<Duruus />}
+          />
+
+          <Route
+            path="/artikel"
+            element={<Articles />}
+          />
+
+          <Route
+            path="/artikel/:slug"
+            element={<ArticleDetails />}
+          />
         </Routes>
       </div>
     </BrowserRouter>
