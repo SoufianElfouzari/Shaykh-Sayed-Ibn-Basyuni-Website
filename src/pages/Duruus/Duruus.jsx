041d4cadcecf4c9a22771ff866089ac3cb87a5a1
Wierss Programm <elfouzari.soufian@gmail.com>
2026-07-25 19:16:52 +0200

    feat: add inital duruus page

---
+++ src/pages/Duruus/Duruus.jsx
@@ -0,0 +1,18 @@
+import Footer from "../Home/common/Footer/Footer";
+import Header from "../Home/common/Header/Header";
+
+function Duruus() {
+  return (
+    <div className="duruus">
+        <Header />
+
+        <main>
+
+        </main>
+
+        <Footer />
+    </div>
+  );
+}
+
+export default Duruus;
