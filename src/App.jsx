38f39fbb75785c10daa20c2952b17993f36ff2ce
Wierss Programm <elfouzari.soufian@gmail.com>
2026-07-31 08:00:27 +0200

    fix: reset scroll position on route navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { useLayoutEffect } from "react";
 import "./App.css";
+
 import {
   BrowserRouter,
   Routes,
   Route,
+  useLocation,
 } from "react-router-dom";
+
 import Home from "./pages/Home/Home";
 import Shaykh from "./pages/Shaykh/Shaykh";
 import Duruus from "./pages/Duruus/Duruus";
@@ -11,13 +15,31 @@ import Articles from "./pages/Articles/Articles";
 import ArticleDetails from "./pages/Articles/components/ArticleDetails/ArticleDetails";
 import Contact from "./pages/Contact/Contact";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "auto",
+    });
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
+
       <div className="app">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={<Home />}
+          />
 
           <Route
             path="/shaykh"
